eeafb61ab7d8c4336c98df4d0a29bcc9197ec652
Niels <niels.koster@portavita.nl>
2022-06-10 20:46:14 +0200

    Add date formatting and future test, plus cleanup

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const weekDays = ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']
-
-// const firstDay = new Date(new Date().setDate(new Date().getDate() - (53*7)))
-// const gridWeek = document.querySelector('.grid-week')
 const gridContainer = document.querySelector('.grid-container')
 let count = 0
+
 for (let col = 0; col < 53; col++){
   const gridWeek = document.createElement('div')
   for (let row = 0; row < 7; row++) {
@@ -11,20 +8,28 @@ for (let col = 0; col < 53; col++){
     const div = document.createElement('div')
     div.className = 'grid-item'
     const now = new Date()
-    const dayOffset = new Date(now.setDate(now.getDate() + (count - 372 + (7-now.getDay()))))
-    // const d = new Date()
-    // d.setDate(d.getDate() - row)
-    const thisId = `day_${weekDays[row]}___${dayOffset}`
-    div.setAttribute('id', thisId);
-    console.log(count)
+    const dayOffset = new Date(
+      now.setDate(now.getDate() + count - 365 - now.getDay())
+    )
+    div.setAttribute('title', dateFormat(dayOffset))
+    if (dateFormat(dayOffset) === dateFormat(new Date)) {
+      div.classList.add('now')
+    }
+    if (isInTheFuture(dayOffset)) {
+      div.classList.remove('grid-item')
+    }
     gridWeek.append(div)
   }
   gridContainer.append(gridWeek)
 }
 
-/*
-var fiveDaysAgo = new Date(new Date().setDate(new Date().getDate() - 5));
- */
+function dateFormat(d) {
+  const date = d.toString().split(' ')
+  return `${date[0]}, ${date[1]} ${date[2]}, ${date[3]}`
+}
 
-const d = new Date()
-d.setDate(d.getDate() - 2)
+function isInTheFuture(date) {
+  const today = new Date()
+  today.setHours(23, 59, 59, 998)
+  return date > today
+}
